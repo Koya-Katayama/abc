b9c19b0d6074167fcf9e59ffc8deda859c501205
Koya Katayama <57083522+Koya-Katayama@users.noreply.github.com>
2020-04-19 23:22:06 +0900

    Create 01.rb

---
+++ ruby/149/c/01.rb
@@ -0,0 +1,18 @@
+# 00の方が速いが、普通に定義通り全部調べるのでも間に合う。回答の速さを競うコンテスト的にはこっちの方がいいのかもしれない。
+
+x = gets.to_i
+
+while true
+    flag = true
+    for i in 2..x-1
+        if x % i == 0
+            flag = false
+            break
+        end
+    end
+    if flag
+        puts x
+        break
+    end
+    x += 1
+end
